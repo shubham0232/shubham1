a1b6f0595b0257aafc9b929c3683ddb3c371735a
shubham0232 <sp9019978974@gmail.com>
2025-02-14 21:47:53 +0530

    Add files via upload

---
+++ for.java
@@ -0,0 +1,18 @@
+package cs;
+import java.util.Scanner;
+public class ifc {
+	public static void main(String []agrs) {
+		//TODO Auto-generated method stud
+		Scanner sc=new Scanner(System.in);
+	
+		System.out.println("enter sem");
+		int sem=sc.nextInt();
+		
+		if(sem==4)
+		{
+			System.out.println("true block sem4");
+		}
+		System.out.println("NEXT statement");
+	}
+
+}
